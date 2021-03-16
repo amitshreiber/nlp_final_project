56a4c9f6b17010ee5e5cfdab8eb4ede9c125c66e
Anat <anatcohen2@mail.tau.ac.il>
2021-03-16 14:41:32 +0200

    main :: save tokenizing pickle

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import torch
 from Tokenizing import Tokenizing
 from embedding import Embedding
-from args import args
 import os
 from classification_net import ClassificationNet
 from upload_embedding_to_dataloader import upload_embedding_to_dataloader
 from train_classification_net import TrainClassificationNet
 from preprocess_data import PreprocessData
 from torch import optim
-ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)))  # This is your Project Root
-PARAMETERS_DIR = os.path.join(ROOT_DIR, 'parameters')
+from handy_function import plot_accuracies, plot_loss
+from args import ROOT_DIR, PARAMETERS_DIR, WEIGHT_DECAY, LR
 
 # set device to GPU
 print("conda environment:", os.environ['CONDA_DEFAULT_ENV'], "\n \n")
@@ -17,34 +16,37 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
 # pre-process data
-args = args()
-data_file = os.path.join(ROOT_DIR, r'data\just_3_artist.csv')
+data_file = os.path.join(ROOT_DIR, r'data\all_songs_nineteen_artists.csv')
 songs_df = PreprocessData(data_file, 512)
 
 # tokenize songs
-tokenizing_path = os.path.join(PARAMETERS_DIR, "songs_token.pt")
+tokenizing_path = os.path.join(PARAMETERS_DIR, "all_songs_token.pt")
 song_token = Tokenizing(songs_df.filtered_df_songs)
 song_token.tokenize_each_song(tokenizing_path=tokenizing_path)
 if song_token.tokenizing_path is None:
     torch.save(song_token.songs_dict, tokenizing_path)
 
 # song embeddings
-embedding_path = os.path.join(PARAMETERS_DIR, "embedding_3_artist.pt")
-embedding_songs = Embedding(tokenizing_data=song_token.songs_dict, device=device, embedding_csv_path=embedding_path)
+embedding_path = os.path.join(PARAMETERS_DIR, "embedding_all_artist.pt")
+embedding_songs = Embedding(tokenizing_data=song_token.songs_dict, device=device, embedding_path=embedding_path)
 embedding_songs.data_embedding()
 if embedding_songs.embedding_path is None:
     torch.save(embedding_songs.songs_features,  embedding_path)
 
 # create dataloader from embeddings
-embedding_dataloaders = upload_embedding_to_dataloader(song_token.df_songs, embedding_songs.songs_features, args)
+embedding_dataloaders = upload_embedding_to_dataloader(song_token.df_songs, embedding_songs.songs_features)
 
 # train classification net
-classification_net = ClassificationNet(args).to(device)
-if args.weight_decay > 0:
-    adam_optimizer = optim.Adam(classification_net.parameters(), lr=args.lr, weight_decay= args.weight_decay )
+classification_net = ClassificationNet().to(device)
+if WEIGHT_DECAY > 0:
+    adam_optimizer = optim.Adam(classification_net.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
 else:
-    adam_optimizer = optim.Adam(classification_net.parameters(), lr=args.lr)
+    adam_optimizer = optim.Adam(classification_net.parameters(), lr=LR)
 training_net = TrainClassificationNet(train_dataloader=embedding_dataloaders.tr_dataloader, optimizer=adam_optimizer,
-                                      device=device,
-                                      net=classification_net, args=args,
+                                      device=device, net=classification_net,
                                       val_dataloader=embedding_dataloaders.val_dataloader)
+
+# plot figures
+plot_accuracies(training_net.train_acc, training_net.val_acc, 'classification')
+plot_loss(training_net.train_loss, 'classification')
+a=1
